dc456183839ca58af6255ac65e39ca61bce88230
Shown Tien <shown@Showns-MacBook-Pro-3.local>
2014-10-28 16:51:19 +0800

    version 0.0.2
    - add level up

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 monster
 =======
-version 0.0.1
+version 0.0.2
+
+- add level up
--- player.rb
+++ player.rb
@@ -1,10 +1,26 @@
 class Player
-  attr_accessor :name, :attack, :armor, :hp
-  
+  attr_accessor :name, :attack, :armor, :hp, :residual_hp,
+                :exp, :exp_for_next_level, :level
+
   def initialize(name, attack, armor, hp)
     @name   = name
     @attack = attack
     @armor  = armor
     @hp     = hp
+    @residual_hp = @hp
+
+    @level = 1
+    @exp = 0
+    @exp_for_next_level = 20
+  end
+
+  def level_up
+    @level += 1
+
+    @hp += @level * 300
+    @residual_hp = @hp
+
+    @exp = 0
+    @exp_for_next_level += @level * 10
   end
 end
